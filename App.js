b9f996b176b5f3956a384fd68bdb5af7ea0669c4
Yamil-Serafin-Cruz-Ruiz <134110777+Yamil-Serafin-Cruz-Ruiz@users.noreply.github.com>
2024-02-08 23:36:26 -0600

    Add files via upload

---
+++ App.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+
+const Calculator = ({ input, resultado, handleButtonPress }) => {
+  return (
+    <View style={styles.container}>
+      <View style={styles.inputContainer}>
+        <Text style={styles.input}>{input}</Text>
+        <Text style={styles.result}>{resultado}</Text>
+      </View>
+      <View style={styles.buttonsContainer}>
+        {['7', '8', '9', '/', '4', '5', '6', '*', '1', '2', '3', '-', 'C', '0', '=', '+'].map((button, index) => (
+          <TouchableOpacity
+            key={index}
+            style={styles.button}
+            onPress={() => handleButtonPress(button)}
+          >
+            <Text style={styles.buttonText}>{button}</Text>
+          </TouchableOpacity>
+        ))}
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'flex-end',
+  },
+  inputContainer: {
+    backgroundColor: '#f0f0f0',
+    padding: 20,
+    alignItems: 'flex-end',
+  },
+  input: {
+    fontSize: 30,
+  },
+  result: {
+    fontSize: 20,
+    color: 'gray',
+  },
+  buttonsContainer: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+  },
+  button: {
+    width: '25%',
+    backgroundColor: '#dcdcdc',
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 20,
+    borderWidth: 1,
+    borderColor: '#fff',
+  },
+  buttonText: {
+    fontSize: 24,
+  },
+});
+
+export default Calculator;
